19cf2e85fe031664e8921ea116951011fcc5e259
GLOBANT\lucas.csulak <lucas.x.csulak.-nd@disney.com>
2018-11-19 19:23:17 -0300

    video 3 de 8 componentes y distitnas formas de instanciarlos

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import Welcome from './Welcome.js'
 
 class App extends Component {
   render() {
     const names = ["Pedro", "Juan", "Luquitas"]
     return (
       <div>
-        <h1>Hola Mundo</h1>
+        <div>
+          <Welcome name="Marina" />
+          <Welcome name="Saccal" />
+        </div>
+
+        <div>
+          {names.map(name =>
+            <Welcome name={name} />
+          )}
+        </div>
 
         <ul>
           {names.map(name =>
             <li>{name}</li>
           )}
         </ul>
-
       </div>
     );
+
   }
 }
-
 export default App;
