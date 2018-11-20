4b64499e7253634b221d4c817b2960b4379d6884
GLOBANT\lucas.csulak <lucas.x.csulak.-nd@disney.com>
2018-11-20 19:12:51 -0300

    video 8 de 8, explicacion de como usar routing. instalamos: npm install --save react-router y luego vi que fallaba e instalé la version npm i -S react-router@3.2.1 y con esto pude hacer el curso sin problemas

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
 import React, { Component } from 'react';
+import { Router, Route, browserHistory, IndexRoute, Link } from 'react-router'
 import './App.css';
-import Welcome from './Welcome.js'
+//esto en la vida real deberia estar C/U en un archivo separado
+const Home = () => <h2>Home</h2>
+const About = () => <h2>About</h2>
+const NoMatch = () => <h1>Pagina no encontrada</h1>
+const Message = (props) => <h1>Mensaje {props.params.id}</h1>
+
+const Template = (props) => {
+  return (
+    <div>
+      <h1>Mi super app jojo</h1>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="about">About</Link></li>
+      </ul>
+      {props.children}
+    </div>
+  )
+}
 
 class App extends Component {
-  constructor() {
-    super()
-    this.changeTitle = this.changeTitle.bind(this)
-    this.state = {
-      title: "Hola mundo desde Estado"
-
-    }
-  }
-
-
   render() {
     return (
       <div>
-        <h1>{this.state.title}</h1>
-        <button onClick={this.changeTitle}>Cambie el titulo</button>
+        <Router history={browserHistory}>
+
+          <Route path="/" component={Template}>
+            <IndexRoute component={Home} />
+            <Route path="about" component={About} />
+          </Route>
+          <Route path="messages/:id" component={Message} />
+          <Route path="*" component={NoMatch} />
+        </Router>
       </div>
     )
   }
-
-  changeTitle() {
-    this.setState({
-      title: "nuevo titulo"
-    })
-  }
 }
+
+
 
 export default App;
